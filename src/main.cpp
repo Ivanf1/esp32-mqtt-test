ffeb5d3102c8052850e13c6f7d7905eb40639d98
Ivan <capobianco.ivan@gmail.com>
2022-09-17 11:28:27 +0200

    refactor: use logging instead of serial print

--- src/main.cpp
+++ src/main.cpp
@@ -4,72 +4,68 @@
 
 #include "secrets.h"
 
-const char *mqtt_broker = BROKER_IP;
+#define WAIT_TIME_BEFORE_CONNECTION_RETRY 5000
+
+const char *TAG = "MQTT Client";
+
 const char *topic = "esp32/test";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
 void callback(char *topic, byte *payload, unsigned int length) {
-  Serial.print("Message arrived in topic: ");
-  Serial.println(topic);
-  Serial.print("Message:");
+  ESP_LOGD(TAG, "Message arrived in topic: %s\nMessage:", topic);
   for (int i = 0; i < length; i++) {
-      Serial.print((char) payload[i]);
+    ESP_LOGD(TAG, "%s", (char) payload[i]);
   }
-  Serial.println();
-  Serial.println("-----------------------");
+  ESP_LOGD(TAG, "\n");
 }
 
-void reconnect() {
+void reconnect_mqtt() {
   // Loop until we're reconnected
   while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
+    ESP_LOGD(TAG, "Attempting MQTT connection");
     // Attempt to connect
     if (client.connect("arduinoClient")) {
-      Serial.println("connected");
+      ESP_LOGD(TAG, "MQTT connection established");
+
       // Once connected, publish an announcement...
-      client.publish("outTopic","hello world");
+      client.publish(topic,"hello world");
       // ... and resubscribe
-      client.subscribe("inTopic");
+      client.subscribe(topic);
+
     } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
+      ESP_LOGE(TAG, "MQTT connection failed, status=%d\nTry again in %d seconds", client.state(), WAIT_TIME_BEFORE_CONNECTION_RETRY);
+      delay(WAIT_TIME_BEFORE_CONNECTION_RETRY);
     }
   }
 }
 
 void setup() {
   Serial.begin(115200);
-  Serial.print("Connecting to ");
-  Serial.println(SSID);
+  ESP_LOGD(TAG, "Connecting to WiFi SSID: %s", SSID);
  
   WiFi.mode(WIFI_STA);
   WiFi.begin(SSID, PWD);
 
-  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.printf("WiFi Failed!\n");
-    return;
-  } else {
-    Serial.println("WiFi connected\n");
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    ESP_LOGE(TAG, "WiFi connection failed, status=%d\nTry again in %d seconds", WiFi.status(), WAIT_TIME_BEFORE_CONNECTION_RETRY);
+    delay(WAIT_TIME_BEFORE_CONNECTION_RETRY);
   }
-  Serial.println(WiFi.localIP());
 
-  Serial.print("Connected.");
+  ESP_LOGD(TAG, "WiFi connection established\n");
+  ESP_LOGD(TAG, "%s", WiFi.localIP().toString());
 
-  client.setServer(mqtt_broker, 1883);
+  client.setServer(BROKER_IP, BROKER_PORT);
   client.setCallback(callback);
 }
 
 void loop() {
   if (!client.connected()) {
-    reconnect();
+    reconnect_mqtt();
   } else {
-    client.publish(topic, "Hello");
-    client.subscribe(topic);
+    // client.publish(topic, "Hello");
+    // client.subscribe(topic);
   }
   client.loop();
 }
